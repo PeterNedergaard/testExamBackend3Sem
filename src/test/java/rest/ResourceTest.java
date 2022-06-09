9de8992814b46bdc4ea8f1eba301b6291ab310b4
Peter Nedergaard <peter@nedergaard.org>
2022-06-09 17:05:29 +0200

    delete tests

--- src/test/java/rest/ResourceTest.java
+++ src/test/java/rest/ResourceTest.java
@@ -65,24 +65,4 @@ public class ResourceTest {
         given().when().get("/info").then().statusCode(200);
     }
 
-    //This test assumes the database contains two rows
-    @Test
-    public void testDummyMsg() throws Exception {
-        given()
-                .contentType("application/json")
-                .get("/xxx/").then()
-                .assertThat()
-                .statusCode(HttpStatus.OK_200.getStatusCode())
-                .body("msg", equalTo("Hello World"));
-    }
-
-    @Test
-    public void testCount() throws Exception {
-        given()
-                .contentType("application/json")
-                .get("/xxx/count").then()
-                .assertThat()
-                .statusCode(HttpStatus.OK_200.getStatusCode())
-                .body("count", equalTo(2));
-    }
 }
